a28b5e3d427ab1c037e96008bda7de1e307f7afe
wangshuo <wang46906@msn.cn>
2020-09-04 19:25:40 +0800

    ss

--- python_file/test1111.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-import datetime
-from time import sleep
-dict1=dict(a=123,b=234)
-dict1.clear()
-print(dict1)
--- python_file/test2222.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-import test1111
-while True:
-    print(test1111.x)
