9d7d6ca88a33bcb8cca44d251281e49e02397826
王硕 <wangshuo@wangshuodeMacBook-Pro.local>
2021-04-01 17:31:41 +0800

    2021/4/1

--- TTT.py
+++
@@ -1,4 +0,0 @@
-
-f1=open("thrift_command/r1.txt",mode="a")
-f1.write("\ntable_add export_timestamp_t export_timestamp => 0")
-f1.close
--- test.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-import datetime
-from concurrent.futures import ProcessPoolExecutor
-
-
-print(type(list()))
